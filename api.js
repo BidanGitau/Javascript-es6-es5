b387f89c303823e08add4d63b6c1b1e091e3bc71
Bidan Gitau <bydangitau@gmail.com>
2020-02-26 17:39:47 +0300

    playing around with promises

--- api.js
+++ api.js
@@ -1,1 +1,40 @@
-console.log('Hello world');
+//http request
+
+const getData = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+            // console.log(request, request.readyState);
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject('cant fetch data');
+            }
+        });
+        request.open('GET', resource);
+        request.send();
+    });
+};
+// getTodo((err, data) => {
+//     console.log('callback fired');
+//     console.log(err, data);
+// });
+//promises
+// const getData = () => {
+//     return new Promise((resolve, reject) => {
+//         //fetch data
+//         // resolve('some data');
+//         reject('some error')
+//     });
+
+// };
+
+getData('tasks/build.json').then((data) => {
+    console.log(data);
+    return getData('tasks/todos.json');
+}).then((data) => {
+    console.log('second promise 2', data);
+}).catch(err => {
+    console.log(err);
+});
